aa7cd68beb7a8313512d59a037ab3c040740e15b
clement-hervouet <clement.hervouet57@laposte.net>
2026-02-27 16:24:54 +0100

    UPDATE: Base

--- static/css/darkmode.js
+++ static/css/darkmode.js
@@ -1,23 +1,19 @@
 let darkmode = localStorage.getItem('darkmode');
-const themeSwitch = document.getElementById('theme-switch')
-
-const githubIcon = document.getElementById('github_icon')
+const themeSwitch = document.getElementById('theme-switch');
 
 const enableDarkmode = () => {
-    document.body.classList.add('darkmode')
-    localStorage.setItem('darkmode', 'active')
-
-    githubIcon.setAttribute('fill', 'white');
+    document.body.classList.add('darkmode');
+    localStorage.setItem('darkmode', 'active');
 }
 
 const disableDarkmode = () => {
-    document.body.classList.remove('darkmode')
-    localStorage.setItem('darkmode', null)
+    document.body.classList.remove('darkmode');
+    localStorage.setItem('darkmode', null);
 }
 
 if (darkmode === "active") enableDarkmode()
 
 themeSwitch.addEventListener("click", () => {
     darkmode = localStorage.getItem('darkmode');
-    darkmode !== "active" ? enableDarkmode() : disableDarkmode() 
+    darkmode !== "active" ? enableDarkmode() : disableDarkmode();
 })
